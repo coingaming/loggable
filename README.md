2d9b0f0f1995164b0c7494a0266a455ed10ade45
Ilja Tkachuk <ilja@heathmont.net>
2019-08-21 14:52:02 +0300

    readme

--- README.md
+++ README.md
@@ -1,11 +1,50 @@
 # Loggable
 
-**TODO: Add description**
+Sometimes it's needed to hide some chunks of data before applying Logger functions. For example credit card numbers, passwords and other sensitive information. Loggable protocol helps us to do it for any type of data. It has fallback to `Any` (does not do anything by default).
+
+<img src="priv/img/logo.jpg" width="300"/>
+
+## Usage
+
+```elixir
+defmodule User do
+  @enforce_keys [:name, :balance, :card]
+  defstruct @enforce_keys
+end
+
+defimpl Loggable.Protocol, for: User do
+  def show(%User{} = x) do
+    %User{x | card: "[SECRET]"}
+  end
+end
+```
+
+and then
+
+```elixir
+iex> x = %User{
+...>   name: "Jessy",
+...>   balance: 100,
+...>   card: "4510 6459 8301 6543"
+...> }
+%User{balance: 100, card: "4510 6459 8301 6543", name: "Jessy"}
+
+iex> Loggable.show(x)
+%User{balance: 100, card: "[SECRET]", name: "Jessy"}
+
+iex> Loggable.show(%{foo: [x, x, x]})
+%{
+  foo: [
+    %User{balance: 100, card: "[SECRET]", name: "Jessy"},
+    %User{balance: 100, card: "[SECRET]", name: "Jessy"},
+    %User{balance: 100, card: "[SECRET]", name: "Jessy"}
+  ]
+}
+```
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `loggable` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `loggable` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -14,8 +53,3 @@ def deps do
   ]
 end
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/loggable](https://hexdocs.pm/loggable).
-
--- mix.exs
+++ mix.exs
@@ -4,11 +4,47 @@ defmodule Loggable.MixProject do
   def project do
     [
       app: :loggable,
-      version: "0.1.0",
+      version: "VERSION" |> File.read!() |> String.trim(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      consolidate_protocols: Mix.env() != :test,
-      deps: deps()
+      consolidate_protocols: not (Mix.env() in [:dev, :test]),
+      deps: deps(),
+      aliases: aliases(),
+      # excoveralls
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.travis": :test,
+        "coveralls.circle": :test,
+        "coveralls.semaphore": :test,
+        "coveralls.post": :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test
+      ],
+      # dialyxir
+      dialyzer: [
+        ignore_warnings: ".dialyzer_ignore",
+        plt_add_apps: [
+          :mix,
+          :ex_unit
+        ]
+      ],
+      # ex_doc
+      name: "Loggable",
+      source_url: "https://github.com/coingaming/loggable",
+      homepage_url: "https://github.com/coingaming/loggable",
+      docs: [main: "readme", extras: ["README.md"]],
+      # hex.pm stuff
+      description: "Protocol for custom views of data in logs (hide sensitive data etc)",
+      package: [
+        licenses: ["MIT"],
+        files: ["lib", "priv", "mix.exs", "README*", "VERSION*"],
+        maintainers: ["ILJA TKACHK aka timCF"],
+        links: %{
+          "GitHub" => "https://github.com/coingaming/loggable",
+          "Author's home page" => "https://itkach.uk/"
+        }
+      ]
     ]
   end
 
@@ -22,8 +58,18 @@ defmodule Loggable.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # development tools
+      {:excoveralls, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.9", only: [:dev, :test], runtime: false},
+      {:boilex, "~> 0.2", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      docs: ["docs", "cmd mkdir -p doc/priv/img/", "cmd cp -R priv/img/ doc/priv/img/", "docs"]
     ]
   end
 end
